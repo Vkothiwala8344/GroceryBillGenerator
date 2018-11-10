326634ff6795b5af3e6f2ec3eac8d15745abfc81
vkoth <vkothiwala6@gmail.com>
2018-11-10 12:42:16 -0500

    with butterknife api applied on main activity

--- app/src/main/java/com/stepintoit/vkoth/grocerybillgenerator/MainActivity.java
+++ app/src/main/java/com/stepintoit/vkoth/grocerybillgenerator/MainActivity.java
@@ -8,131 +8,55 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
 
     static int cApple = 0, cGrapes = 0, cBanana = 0;
-    Button btnSubmit, btnIncrApple, btnDecrApple, btnIncrGrapes, btnDecrGrapes, btnIncrBanana, btnDecrBanana;
-    TextView tvCountApple, tvCountGrapes, tvCountBanana;
+
+    @BindView(R.id.btn_increase_apple)
+    Button btnIncrApple;
+    @BindView(R.id.btn_decrease_apple)
+    Button btnDecrApple;
+    @BindView(R.id.btn_increase_grapes)
+    Button btnIncrGrapes;
+    @BindView(R.id.btn_decrease_grapes)
+    Button btnDecrGrapes;
+    @BindView(R.id.btn_increase_banana)
+    Button btnIncrBanana;
+    @BindView(R.id.btn_decrease_banana)
+    Button btnDecrBanana;
+    @BindView(R.id.btn_submit)
+    Button btnSubmit;
+
+    @BindView(R.id.tv_count_apple)
+    TextView tvCountApple;
+    @BindView(R.id.tv_count_grapes)
+    TextView tvCountGrapes;
+    @BindView(R.id.tv_count_banana)
+    TextView tvCountBanana;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-        btnSubmit = (Button) findViewById(R.id.btn_submit);
-        btnIncrApple = (Button) findViewById(R.id.btn_increase_apple);
-        btnDecrApple = (Button) findViewById(R.id.btn_decrease_apple);
-        btnIncrGrapes = (Button) findViewById(R.id.btn_increase_grapes);
-        btnDecrGrapes = (Button) findViewById(R.id.btn_decrease_grapes);
-        btnIncrBanana = (Button) findViewById(R.id.btn_increase_banana);
-        btnDecrBanana = (Button) findViewById(R.id.btn_decrease_banana);
-
-        tvCountApple = (TextView) findViewById(R.id.tv_count_apple);
-        tvCountGrapes = (TextView) findViewById(R.id.tv_count_grapes);
-        tvCountBanana = (TextView) findViewById(R.id.tv_count_banana);
-
-        btnSubmit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                if (cApple < 0 || cGrapes < 0 || cBanana < 0) {
-                    Toast.makeText(getApplicationContext(), "item number can not be negative", Toast.LENGTH_SHORT).show();
-                } else {
-
-                    Product pApple = new Product();
-                    pApple.setProductId(1);
-                    pApple.setProductName("Apple");
-                    pApple.setProductPrice(3);
-                    pApple.setProductQuantity(cApple);
-
-                    Product pGrapes = new Product();
-                    pGrapes.setProductId(2);
-                    pGrapes.setProductName("Grapes");
-                    pGrapes.setProductPrice(5);
-                    pGrapes.setProductQuantity(cGrapes);
-
-                    Product pBanana = new Product();
-                    pBanana.setProductId(3);
-                    pBanana.setProductName("Banana");
-                    pBanana.setProductPrice(4);
-                    pBanana.setProductQuantity(cBanana);
-
-                    Intent i = new Intent(getApplicationContext(), InvoiceActivity.class);
-                    Bundle bundle = new Bundle();
-                    bundle.putSerializable("apple", pApple);
-                    bundle.putSerializable("grapes", pGrapes);
-                    bundle.putSerializable("banana", pBanana);
-                    i.putExtras(bundle);
-                    startActivity(i);
-
-
-                }
-            }
-        });
-
-        btnIncrApple.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                cApple++;
-                tvCountApple.setText(Integer.toString(cApple));
-            }
-        });
-
-        btnDecrApple.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                cApple--;
-                tvCountApple.setText(Integer.toString(cApple));
-            }
-        });
-
-        btnIncrGrapes.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                cGrapes++;
-                tvCountGrapes.setText(Integer.toString(cGrapes));
-            }
-        });
-
-        btnDecrGrapes.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                cGrapes--;
-                tvCountGrapes.setText(Integer.toString(cGrapes));
-            }
-        });
-
-        btnIncrBanana.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                cBanana++;
-                tvCountBanana.setText(Integer.toString(cBanana));
-            }
-        });
-
-        btnDecrBanana.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                cBanana--;
-                tvCountBanana.setText(Integer.toString(cBanana));
-            }
-        });
-
+        ButterKnife.bind(this);
 
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
-        outState.putInt("appleCount",cApple);
-        outState.putInt("grapesCount",cGrapes);
-        outState.putInt("bananaCount",cBanana);
+        outState.putInt("appleCount", cApple);
+        outState.putInt("grapesCount", cGrapes);
+        outState.putInt("bananaCount", cBanana);
         super.onSaveInstanceState(outState);
     }
 
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        int cApp,cGra,cBan;
+        int cApp, cGra, cBan;
         cApp = savedInstanceState.getInt("appleCount");
         cGra = savedInstanceState.getInt("grapesCount");
         cBan = savedInstanceState.getInt("bananaCount");
@@ -143,4 +67,81 @@ public class MainActivity extends AppCompatActivity {
 
         super.onRestoreInstanceState(savedInstanceState);
     }
+
+    @OnClick(R.id.btn_submit)
+    void onClickSubmit() {
+        if (cApple < 0 || cGrapes < 0 || cBanana < 0) {
+            Toast.makeText(getApplicationContext(), "item number can not be negative", Toast.LENGTH_SHORT).show();
+        } else {
+            openInvoicePage();
+        }
+    }
+
+    @OnClick(R.id.btn_increase_apple)
+    void onClickAppleIncrease() {
+        cApple++;
+        tvCountApple.setText(Integer.toString(cApple));
+    }
+
+    @OnClick(R.id.btn_decrease_apple)
+    void onClickAppleDecrease() {
+        cApple--;
+        tvCountApple.setText(Integer.toString(cApple));
+    }
+
+    @OnClick(R.id.btn_increase_grapes)
+    void onClickGrapesIncrease() {
+        cGrapes++;
+        tvCountGrapes.setText(Integer.toString(cGrapes));
+    }
+
+    @OnClick(R.id.btn_decrease_grapes)
+    void onClickGrapesDecrease() {
+        cGrapes--;
+        tvCountGrapes.setText(Integer.toString(cGrapes));
+    }
+
+    @OnClick(R.id.btn_increase_banana)
+    void onClickBananaIncrease() {
+        cBanana++;
+        tvCountBanana.setText(Integer.toString(cBanana));
+    }
+
+    @OnClick(R.id.btn_decrease_banana)
+    void onClickBananaDecrease() {
+        cBanana--;
+        tvCountBanana.setText(Integer.toString(cBanana));
+    }
+
+    void openInvoicePage() {
+        Product pApple = new Product();
+        pApple.setProductId(1);
+        pApple.setProductName("Apple");
+        pApple.setProductPrice(3);
+        pApple.setProductQuantity(cApple);
+
+        Product pGrapes = new Product();
+        pGrapes.setProductId(2);
+        pGrapes.setProductName("Grapes");
+        pGrapes.setProductPrice(5);
+        pGrapes.setProductQuantity(cGrapes);
+
+        Product pBanana = new Product();
+        pBanana.setProductId(3);
+        pBanana.setProductName("Banana");
+        pBanana.setProductPrice(4);
+        pBanana.setProductQuantity(cBanana);
+
+        Intent i = new Intent(getApplicationContext(), InvoiceActivity.class);
+        Bundle bundle = new Bundle();
+        bundle.putSerializable("apple", pApple);
+        bundle.putSerializable("grapes", pGrapes);
+        bundle.putSerializable("banana", pBanana);
+        i.putExtras(bundle);
+        startActivity(i);
+
+    }
+
+
 }
+
