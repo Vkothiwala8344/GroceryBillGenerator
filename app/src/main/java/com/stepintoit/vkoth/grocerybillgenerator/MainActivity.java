ba94563c6ed65828c4a0deab3cdc86362bf0db38
vkoth <vkothiwala6@gmail.com>
2018-11-10 00:38:35 -0500

    with portrait mode

--- app/src/main/java/com/stepintoit/vkoth/grocerybillgenerator/MainActivity.java
+++ app/src/main/java/com/stepintoit/vkoth/grocerybillgenerator/MainActivity.java
@@ -1,32 +1,98 @@
 package com.stepintoit.vkoth.grocerybillgenerator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    int minteger = 0;
+    static int cApple = 0, cGrapes = 0, cBanana = 0;
+    Button btnSubmit, btnIncrApple, btnDecrApple, btnIncrGrapes, btnDecrGrapes, btnIncrBanana, btnDecrBanana;
+    TextView tvCountApple, tvCountGrapes, tvCountBanana;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        btnSubmit = (Button) findViewById(R.id.btn_submit);
+        btnIncrApple = (Button) findViewById(R.id.btn_increase_apple);
+        btnDecrApple = (Button) findViewById(R.id.btn_decrease_apple);
+        btnIncrGrapes = (Button) findViewById(R.id.btn_increase_grapes);
+        btnDecrGrapes = (Button) findViewById(R.id.btn_decrease_grapes);
+        btnIncrBanana = (Button) findViewById(R.id.btn_increase_banana);
+        btnDecrBanana = (Button) findViewById(R.id.btn_decrease_banana);
+
+        tvCountApple = (TextView) findViewById(R.id.tv_count_apple);
+        tvCountGrapes = (TextView) findViewById(R.id.tv_count_grapes);
+        tvCountBanana = (TextView) findViewById(R.id.tv_count_banana);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (cApple < 0 || cGrapes < 0 || cBanana < 0) {
+                    Toast.makeText(getApplicationContext(), "item number can not be negative", Toast.LENGTH_SHORT).show();
+                } else {
+                    startActivity(new Intent(MainActivity.this, InvoiceActivity.class));
+                }
+            }
+        });
+
+        btnIncrApple.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cApple++;
+                tvCountApple.setText(Integer.toString(cApple));
+            }
+        });
+
+        btnDecrApple.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cApple--;
+                tvCountApple.setText(Integer.toString(cApple));
+            }
+        });
+
+        btnIncrGrapes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cGrapes++;
+                tvCountGrapes.setText(Integer.toString(cGrapes));
+            }
+        });
+
+        btnDecrGrapes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cGrapes--;
+                tvCountGrapes.setText(Integer.toString(cGrapes));
+            }
+        });
+
+        btnIncrBanana.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cBanana++;
+                tvCountBanana.setText(Integer.toString(cBanana));
+            }
+        });
+
+        btnDecrBanana.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cBanana--;
+                tvCountBanana.setText(Integer.toString(cBanana));
+            }
+        });
+
+
     }
 
-    public void increaseInteger(View view) {
-        minteger = minteger + 1;
-        display(minteger);
-
-    }public void decreaseInteger(View view) {
-        minteger = minteger - 1;
-        display(minteger);
-    }
-
-    private void display(int number) {
-        TextView displayInteger = (TextView) findViewById(
-                R.id.tv_count_apple);
-        displayInteger.setText("" + number);
-    }
 }
---
+++ app/src/main/java/com/stepintoit/vkoth/grocerybillgenerator/Product.java
@@ -0,0 +1,9 @@
+package com.stepintoit.vkoth.grocerybillgenerator;
+
+import java.io.Serializable;
+
+public class Product implements Serializable{
+
+    int productId,productPrice,productQuantity;
+    String productName;
+}
